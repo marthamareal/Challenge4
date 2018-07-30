b2a7a00b69d432bd7f443621e414113c887e8eeb
marthamareal <marthamareal@gmail.com>
2018-07-31 00:22:44 +0300

    add requests view and approval

---
+++ ride-my-way/javascript/requests.js
@@ -0,0 +1,136 @@
+
+function getRideRequests(rideId, event) {
+    event.preventDefault();
+
+    if(rideId) {
+        let url = " http://127.0.0.1:5000/rides/requests/" + rideId;
+        let method = 'get';
+        let header = {
+            'Content-Type': 'application/json',
+            'token': localStorage.getItem('token')
+        };
+
+        fetchAPI(url, method, header)
+            .then(results => {
+                if (!results) return;
+
+                //    create table from results and append to DOM
+
+                console.log(results);
+
+                let table =  document.createElement('table');
+
+                let theadRow = document.createElement('tr');
+                console.log(theadRow);
+
+                let tableHeads = ["","DATE", "REF NO", "RIDER", "STATUS", "ACTION"];
+
+                for (let head = 0; head <tableHeads.length; head ++){
+                    let th =  document.createElement('th');
+                    th.innerText = tableHeads[head];
+                    theadRow.appendChild(th);
+                }
+
+                table.appendChild(theadRow);
+
+
+                let tbody = document.createElement("tbody");
+
+                let count = 0;
+
+                results.data["requests"].forEach(function (offer) {
+                    let wantedFields = [];
+
+                    wantedFields.push(offer['date']);
+                    wantedFields.push(offer['ride_ref']);
+                    wantedFields.push(offer['requestor']);
+                    wantedFields.push(offer['status']);
+                    wantedFields.push(offer['request_id']);
+
+                    let row = document.createElement('tr');
+
+                    count++;
+
+                    let column = document.createElement('td');
+                    column.appendChild(document.createTextNode(count.toString()));
+                    row.appendChild(column);
+
+                    for (let feature = 0; feature < wantedFields.length - 1; feature++) {
+                        let columm1 = document.createElement('td');
+                        columm1.appendChild(document.createTextNode(wantedFields[feature]));
+                        row.appendChild(columm1);
+                    }
+
+                    let action = document.createElement('td');
+                    let button1 = document.createElement('button');
+                    button1.setAttribute("id", "accept");
+                    button1.setAttribute("value", wantedFields[wantedFields.length - 1]);
+                    button1.setAttribute("action", "Y");
+                    button1.innerText = "Accept";
+                    button1.setAttribute("class", "approve_request");
+                    action.appendChild(button1);
+
+
+                    let button2 = document.createElement('button');
+                    button2.setAttribute("id", "reject");
+                    button2.setAttribute("value",  wantedFields[wantedFields.length - 1]);
+                    button2.setAttribute("action", "N");
+                    button2.setAttribute("class", "approvedff_request");
+                    button2.innerText = "Reject";
+                    action.appendChild(button2);
+
+                    row.appendChild(action);
+
+                    tbody.appendChild(row);
+
+                    table.appendChild(tbody);
+
+                    localStorage.setItem('rideRequests', table.outerHTML);
+
+                    window.location.href = "../ui/request-list.html";
+
+                });
+            }).catch(function (error) {
+            console.log(error)
+        })
+    }
+}
+
+function approveRequest(requestId,approval, event) {
+
+    event.preventDefault();
+
+    let url = " http://127.0.0.1:5000/rides/requests/approve/" + requestId;
+
+    console.log(url);
+        let method = 'post';
+        let header = {
+            'Content-Type': 'application/json',
+            'token': localStorage.getItem('token')
+        };
+
+        fetchAPI(url, method, header, {"approval": approval})
+            .then(results => {
+            if(!results) return;
+            if (results.status === 201) {
+            console.log("Reached fun");
+
+        }
+            })
+
+
+}
+
+let button = document.getElementsByClassName("approve_request")[0];
+if(button){
+    console.log("okkkkkkkkkkkkkkkkkkkkkkkkk");
+    button.addEventListener("click", function () {
+        approveRequest(button.getAttribute("value"), button.getAttribute("action"))
+    })
+
+}
+
+
+let outerDivShowRideRequests = document.getElementById('ride_requests');
+if (outerDivShowRideRequests)
+    outerDivShowRideRequests.innerHTML = localStorage.getItem('rideRequests');
--- ride-my-way/javascript/rides.js
+++ ride-my-way/javascript/rides.js
@@ -1,6 +1,4 @@
 
-if (document.getElementById('ride_offers')) getOffers();
-
 function getOffers() {
     if (localStorage.getItem('token')) {
         let url = "http://127.0.0.1:5000/rides";
@@ -223,6 +221,74 @@ function viewOffer(offerId, event) {
 
     }
 }
+
+function getDriverOffers() {
+
+    if (localStorage.getItem('token')) {
+        let url = "http://127.0.0.1:5000/driver/rides";
+        let method = 'get';
+        let header = {
+            'Content-Type': 'application/json',
+            'token': localStorage.getItem('token')
+        };
+        fetchAPI(url, method, header)
+            .then(results => {
+                if (!results) return;
+
+                //    create table from results and append to DOM
+
+                let tbody = document.getElementById('my_offers');
+                let count = 0;
+
+                results.data["driver_offers"].forEach(function (offer) {
+                    let wantedFields = [];
+                    wantedFields.push(offer['date']);
+                    wantedFields.push(offer['source']);
+                    wantedFields.push(offer['destination']);
+                    wantedFields.push(offer['time']);
+                    wantedFields.push(offer['requests_no']);
+                    wantedFields.push(offer['id']);
+
+
+                    let row = document.createElement('tr');
+
+                    count++;
+
+                    let column = document.createElement('td');
+                    column.appendChild(document.createTextNode(count.toString()));
+                    row.appendChild(column);
+
+                    for (let feature = 0; feature < wantedFields.length - 1; feature++) {
+                        let column1 = document.createElement('td');
+                        column1.appendChild(document.createTextNode(wantedFields[feature]));
+                        row.appendChild(column1);
+                    }
+
+                    let action = document.createElement('td');
+                    let button = document.createElement('button');
+                    button.setAttribute("id", "accept");
+                    button.setAttribute("value", wantedFields[wantedFields.length - 1]);
+                    button.addEventListener("click", function () {
+                        console.log(this.value);
+                        // history.pushState({"offerId": this.value}, "Requests","../ui/request-list.html");
+
+                        getRideRequests(this.value, event)
+                    });
+                    button.innerText = "View Requests";
+                    action.appendChild(button);
+                    row.appendChild(action);
+
+                    tbody.appendChild(row)
+                });
+            }).catch(function (error) {
+            console.log(error)
+        })
+    }
+}
+
+if (document.getElementById('ride_offers')) getOffers();
+
+if (document.getElementById('driver_offers')) getDriverOffers();
 
 let outerDivShowingRide = document.getElementById('show-ride-details');
 if (outerDivShowingRide)
